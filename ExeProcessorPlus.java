7b28e82ad92d9d0d6c51bc615ef6f7115387898b
Brian <985681391@qq.com>
2022-05-05 09:01:31 +0800

    可执行包调用并行处理优化

--- ExeProcessorPlus.java
+++ ExeProcessorPlus.java
@@ -7,8 +7,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.Vector;
 
-enum ExeProcessorType{
-	PRINT,EXPORT,OTHERS
+enum ExeProcessorType {
+	PRINT, EXPORT, OTHERS
 }
 
 public class ExeProcessorPlus {
@@ -32,9 +32,9 @@ public class ExeProcessorPlus {
 		this.argvs = commandStr;
 	}
 
-	private void dealPipeData(String data, ExeProcessorType type,BufferedWriter writer) {
+	private void dealPipeData(String data, ExeProcessorType type, BufferedWriter writer) {
 		try {
-			switch(type) {
+			switch (type) {
 			case PRINT:
 				System.out.println(data);
 				break;
@@ -51,8 +51,71 @@ public class ExeProcessorPlus {
 		}
 	}
 
-	public boolean executeRun(String path, Vector<String> argvs, 
-			ExeProcessorType type,String exportPath, boolean isParallel) {
+	private void internalExecute(ExeProcessorType type,String exportPath) {
+		try {
+			// 内部使用对象，来完成可能的对外txt文件输出
+			File dstFile = null;
+			FileWriter dstFileWriter = null;
+			BufferedWriter dstbWriter = null;
+
+			if (type == ExeProcessorType.EXPORT) {
+				dstFile = new File(exportPath);
+				dstFile.createNewFile();
+				dstFileWriter = new FileWriter(dstFile);
+				dstbWriter = new BufferedWriter(dstFileWriter);
+			}
+
+			// 从管道中读出输出信息
+			if (dstbWriter != null) {
+				dstbWriter.write("输出信息");
+				dstbWriter.newLine();
+				dstbWriter.newLine();
+			} else {
+				System.out.println("输出信息");
+			}
+			BufferedReader stdOut = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+			for (String line; null != (line = stdOut.readLine());)
+				dealPipeData(line, type, dstbWriter);
+
+			// 从管道中读错误信息
+			if (dstbWriter != null) {
+				dstbWriter.write("错误信息");
+				dstbWriter.newLine();
+				dstbWriter.newLine();
+			} else {
+				System.out.println("错误信息");
+			}
+
+			BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+			for (String line; null != (line = stdError.readLine());)
+				dealPipeData(line, type, dstbWriter);
+
+			if (dstbWriter != null) {
+				dstbWriter.close();
+			}
+
+			if (dstFileWriter != null) {
+				dstFileWriter.close();
+			}
+
+			if (stdOut != null) {
+				stdOut.close();
+			}
+
+			if (stdError != null) {
+				stdError.close();
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
+	public boolean executeRun(String path, Vector<String> argvs, ExeProcessorType type, String exportPath,
+			boolean isParallel) {
 		try {
 			if (path.contains(" ")) {
 				path = "\"" + path + "\"";
@@ -81,77 +144,18 @@ public class ExeProcessorPlus {
 			 * "\n").getBytes()); } catch (Exception ex) { ex.printStackTrace(); } } }
 			 * }.start();
 			 */
-
-			new Thread() {
-				public void run() {
-					try {
-						// 内部使用对象，来完成可能的对外txt文件输出
-						File dstFile = null;
-						FileWriter dstFileWriter = null;
-						BufferedWriter dstbWriter = null;
-
-						if (type==ExeProcessorType.EXPORT) {
-							dstFile = new File(exportPath);
-							dstFile.createNewFile();
-							dstFileWriter = new FileWriter(dstFile);
-							dstbWriter = new BufferedWriter(dstFileWriter);
-						}
-
-						// 从管道中读出输出信息
-						if (dstbWriter!=null) {
-							dstbWriter.write("输出信息");
-							dstbWriter.newLine();
-							dstbWriter.newLine();
-						}
-						else {
-							System.out.println("输出信息");
-						}
-						BufferedReader stdOut = new BufferedReader(
-								new InputStreamReader(process.getInputStream()));
-
-						for (String line; null != (line = stdOut.readLine());)
-							dealPipeData(line, type,dstbWriter);
-
-						// 从管道中读错误信息				
-						if (dstbWriter!=null) {
-							dstbWriter.write("错误信息");
-							dstbWriter.newLine();
-							dstbWriter.newLine();
-						}
-						else {
-							System.out.println("错误信息");
-						}
-						
-						
-						BufferedReader stdError = new BufferedReader(
-								new InputStreamReader(process.getErrorStream()));
-
-						for (String line; null != (line = stdError.readLine());)
-							dealPipeData(line, type,dstbWriter);
-
-						if (dstbWriter != null) {
-							dstbWriter.close();
-						}
-
-						if (dstFileWriter != null) {
-							dstFileWriter.close();
-						}
-
-						if (stdOut != null) {
-							stdOut.close();
-						}
-
-						if (stdError != null) {
-							stdError.close();
-						}
-
-					} catch (Exception e) {
-						// TODO: handle exception
-						e.printStackTrace();
-					}
-
-				};
-			}.start();
+			
+			if (isParallel) {
+				new Thread() {
+					public void run() {
+						internalExecute(type, exportPath);
+					};
+				}.start();
+			}
+			else {
+				internalExecute(type, exportPath);
+			}
+			
 
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -168,7 +172,7 @@ public class ExeProcessorPlus {
 		argvs.add("123");
 		argvs.add("456");
 
-		exeProcessorPlus.executeRun("exeDemo/test.exe", argvs, ExeProcessorType.PRINT,"", true);
+		exeProcessorPlus.executeRun("exeDemo/test.exe", argvs, ExeProcessorType.PRINT, "", true);
 
 		while (true) {
 			System.out.println("这是主线程");
